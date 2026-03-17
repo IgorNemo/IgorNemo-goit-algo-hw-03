34aa9c8be57d80571ca72039788b5f81d3ec91d4
Igor Danyliuk <danig@ukr.net>
2026-03-17 13:02:15 +0200

    Repairing_17.03.2026

--- HomeTask2.py
+++ HomeTask2.py
@@ -1,15 +1,16 @@
 import random
 
 def get_numbers_ticket(min_number, max_number, quantity_numbers):
-    
     s = set()
-    while len(s) < quantity_numbers:
-        s.add(random.randint(min_number, max_number))
-    s = sorted(s)
+    if min_number >= 1:
+        if max_number <= 1000:
+            while len(s) < quantity_numbers:
+                s.add(random.randint(min_number, max_number))
+            s = sorted(s)
     return s
   
-min = 1
-max = 200
+min = 3
+max = 100000
 quantity = 5
 ss = get_numbers_ticket(min, max, quantity)
 print(ss)
